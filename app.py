333e9539daad3123695a3b49756bedb4f830f482
azizbekkhabibullayev <azkhabibullayev@gmail.com>
2023-12-06 16:01:19 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+# def sozlarni_tanlash(matn, separator=' '):
+   
+    
+#     sozlar = matn.split(separator)
+#     return sozlar
+
+
+# matn = str(input('sozlarni kiriting'))
+# sozlar_royxati = sozlarni_tanlash(matn)
+
+# print("Matn:", matn)
+# print("So'zlar ro'yxati:", sozlar_royxati)
+# 2-massala
+# def unli_harflar_soni(matn):
+   
+#     unli_harflar = "aeiouo'AEIOUO'"
+#     son = sum(1 for i in matn if i in unli_harflar)
+#     return son
+
+# matn =str(input("soz kiriting va men unli harflarni sanab beraman"))
+
+
+# print("Matn:", matn)
+# print("Unli harflar soni:", unli_harflar_soni(matn))
+# 3-masala
+# def nol_soni(noll):
+#     son = noll.count('0')
+#     return son
+
+
+# raqam = str(input('sonlar kiriting'))
+
+# print("Raqam:", raqam)
+# print("0 larning soni:",nol_soni(raqam))
+
+# def unli_harflarni_teashirish(matn1, matn2):
+    
+#     unli_harflar = "aeiouAEIOU"
+#     unli_harf_royxati1 = [harf1 for harf1 in matn1 if harf1 in unli_harflar]
+#     unli_harf_royxati2 = [harf2 for harf2 in matn2 if harf2 in unli_harflar]
+   
+#     if unli_harf_royxati1==unli_harf_royxati2:
+#         return True
+#     else:False
+#     return False
+
+
+# matn1 = "Salom "
+# matn2 = "Salom "
+
+
+# print(f'Matn:, {matn1},\nikkinchi matn:{matn2}')
+# print( unli_harflarni_teashirish(matn1,matn2))
+
+
+
